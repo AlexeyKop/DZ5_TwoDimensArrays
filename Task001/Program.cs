fc1af442e81f43264a3d7e9c32a2ab9ea0f01121
AlexeyKop <a.kop@bk.ru>
2023-12-05 01:03:31 +0400

    HomeWork for Seminar 5

---
+++ Task001/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача  1: Напишите  программу,  которая  на  вход принимает  позиции  элемента  
+//в  двумерном  массиве,  и возвращает  значение  этого  элемента  
+//или  же  указание, что такого элемента нет.
+
+void InputMatrix(int[,] matrix){
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix){
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int FindElemArray (int x, int y, int [,] matrix){
+    int findelem = 0;    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i  == x)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j  == y)
+            findelem = matrix[i, j];       
+        }        
+    }
+    return findelem;
+}
+
+Console.Clear();
+Console.Write("Введ размеры двумерн массива через пробел: ");
+int [] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int [,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Begining array: ");
+PrintMatrix(matrix);
+Console.WriteLine("Введ кординаты элемента в двумерн массиве через Enter: ");
+int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+
+int n = FindElemArray(x, y, matrix);
+if (n != 0)
+    Console.WriteLine($"Элемент с координатами {x}, {y}: {FindElemArray(x, y, matrix)} ");
+else
+    Console.WriteLine("Такого элемента не существует");
---
+++ Task003/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача  3: Задайте  прямоугольный  двумерный  массив. Напишите  программу,  
+//которая  будет  находить  строку  с наименьшей суммой элементов.
+void InputMatrix(int[,] matrix){
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix){
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int [] SumStrArray(int [,] matrix){
+    int [] str_array = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int summa = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            summa += matrix[i, j];       
+        }
+        str_array[i] = summa;
+    }
+    return str_array;
+}
+
+int MinIndex(int [] min_array){
+    int min = min_array[0];
+    int minindex = 0;
+    for (int i = 0; i < min_array.Length - 1; i++){
+        if (min > min_array[i]){
+            min = min_array[i];
+            minindex = i;
+        }        
+    }
+    return minindex;
+}
+
+Console.Clear();
+Console.Write("Введ размеры двумерн массива: ");
+int [] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+
+int [,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Begining array: ");
+PrintMatrix(matrix);
+int [] SumStringArray = SumStrArray(matrix);
+Console.WriteLine($"Сум строк в виде одномерн массива: [{string.Join(", ", SumStringArray)}]");
+Console.WriteLine($"Index of str with min sum: {MinIndex(SumStringArray)} ");
+
